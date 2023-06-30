7741aba041d057082505e674bcab3259764c887a
Jihong Min <jihong@csap.snu.ac.kr>
2023-06-30 09:58:28 +0900

    viogpudo: force WDDM v1.3 and internal display type
    See https://gitlab.freedesktop.org/spice/win32/qxl-wddm-dod/-/issues/12.

--- viogpudo/driver.cpp
+++ viogpudo/driver.cpp
@@ -51,7 +51,7 @@ DriverEntry(
     // Initialize DDI function pointers and dxgkrnl
     KMDDOD_INITIALIZATION_DATA InitialData = {0};
 
-    InitialData.Version = DXGKDDI_INTERFACE_VERSION_WIN8;
+    InitialData.Version = DXGKDDI_INTERFACE_VERSION_WDDM1_3;
 
     InitialData.DxgkDdiAddDevice                    = VioGpuDodAddDevice;
     InitialData.DxgkDdiStartDevice                  = VioGpuDodStartDevice;
